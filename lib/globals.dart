a0f0927aa0cfc9e521d5d890f9d4bed46157ab91
Yann Picard <yann.picard95@gmail.com>
2019-01-06 19:49:21 +0800

    Add signup state

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,4 +1,5 @@
 library newyears_flutter.globals;
 
-String base_url = "https://0913f723.ngrok.io";
-String login_url = base_url + "/api/v1/auth/sign_in";
+String base_url = "https://f58007c0.ngrok.io";
+String login_url = base_url + "/api/v1/auth/login";
+String signup_url = base_url + "/api/v1/auth/signup";
--- lib/main.dart
+++ lib/main.dart
@@ -10,56 +10,89 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Startup Name Generator'),
-        ),
-        body: Login(),
-      ),
+      routes: <String, WidgetBuilder>{
+        '/': (BuildContext context) => Home(),
+        '/login': (BuildContext context) => Login()
+      },
     );
   }
 }
 
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HOME TITLE'),
+      ),
+      body: new FlatButton(
+          onPressed: () => Navigator.of(context).pushNamed("/login"),
+          child: new Text("AUTH SCREEN")),
+    );
+  }
+}
+
+class Login extends StatefulWidget {
+  @override
+  LoginState createState() {
+    return LoginState();
+  }
+}
 
 class LoginState extends State<Login> {
   final _formKey = GlobalKey<FormState>();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
   String _email;
   String _password;
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: <Widget>[
-          TextFormField(
-            decoration: InputDecoration(labelText: "Email"),
-            onSaved: (val) {
-              _email = val;
-            },
-          ),
-          TextFormField(
-            decoration: InputDecoration(labelText: "Password"),
-            onSaved: (val) {
-              _password = val;
-            },
-          ),
-          RaisedButton(
-            onPressed: () {
-              _formKey.currentState.save();
-              login(_email, _password);
-            },
-            child: Text("Login"),
-          )
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      key: _scaffoldKey,
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(labelText: "Email"),
+              onSaved: (val) {
+                _email = val;
+              },
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: "Password"),
+              onSaved: (val) {
+                _password = val;
+              },
+            ),
+            RaisedButton(
+              onPressed: () {
+                _formKey.currentState.save();
+                _login(_email, _password);
+              },
+              child: Text("Login"),
+            ),
+            RaisedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Signup()),
+                );
+              },
+              child: Text("Create an account"),
+            )
+          ],
+        ),
       ),
     );
-    ;
   }
 
-  Future<http.Response> login(email, password) async {
-    Scaffold.of(context).showSnackBar(SnackBar(
+  Future<http.Response> _login(email, password) async {
+    _scaffoldKey.currentState.showSnackBar(SnackBar(
       content: Text("Logging in..."),
     ));
 
@@ -67,7 +100,7 @@ class LoginState extends State<Login> {
         body: {"email": email, "password": password}).then((response) {
       String _message = json.decode(response.body)['message'];
 
-      Scaffold.of(context).showSnackBar(SnackBar(
+      _scaffoldKey.currentState.showSnackBar(SnackBar(
         content: Text(_message),
       ));
     });
@@ -75,10 +108,83 @@ class LoginState extends State<Login> {
   }
 }
 
-class Login extends StatefulWidget {
+class Signup extends StatefulWidget {
   @override
-  LoginState createState() {
-    // TODO: implement createState
-    return new LoginState();
+  SignupState createState() {
+    return SignupState();
+  }
+}
+
+class SignupState extends State<Signup> {
+  final _formKey = GlobalKey<FormState>();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  String _firstName;
+  String _lastName;
+  String _email;
+  String _password;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("SIGNUP")),
+      key: _scaffoldKey,
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(labelText: "Name"),
+              onSaved: (val) {
+                _firstName = val;
+              },
+            ),
+            TextFormField(
+                decoration: InputDecoration(labelText: "Last name"),
+                onSaved: (val) {
+                  _lastName = val;
+                }),
+            TextFormField(
+              decoration: InputDecoration(labelText: "Email"),
+              onSaved: (val) {
+                _email = val;
+              },
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: "Password"),
+              onSaved: (val) {
+                _password = val;
+              },
+            ),
+            RaisedButton(
+              onPressed: () {
+                _formKey.currentState.save();
+                _signup(_firstName, _lastName, _email, _password);
+              },
+              child: Text("Create account"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<http.Response> _signup(firstName, lastName, email, password) async {
+    _scaffoldKey.currentState.showSnackBar(SnackBar(
+      content: Text("Creating account..."),
+    ));
+
+    final response = await http.post(globals.signup_url, body: {
+      "firstName": firstName,
+      "lastName": lastName,
+      "email": email,
+      "password": password
+    }).then((response) {
+      String _message = json.decode(response.body)['message'];
+
+      _scaffoldKey.currentState.showSnackBar(SnackBar(
+        content: Text(_message),
+      ));
+    });
+    return response;
   }
 }
