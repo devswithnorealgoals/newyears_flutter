0363de7e10f5ae24777566c4af0e45ddf8e4ce38
Yann Picard <yann.picard95@gmail.com>
2019-01-08 21:54:41 +0800

    Add signup process

--- lib/signup-page.dart
+++ lib/signup-page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'globals.dart' as globals;
 import 'dart:convert';
+import 'user.dart';
 
 class SignupPage extends StatefulWidget {
   @override
@@ -13,73 +14,70 @@ class SignupPage extends StatefulWidget {
 class SignupState extends State<SignupPage> {
   final _formKey = GlobalKey<FormState>();
   final _scaffoldKey = GlobalKey<ScaffoldState>();
-  String _firstName;
-  String _lastName;
-  String _email;
-  String _password;
+  bool userCreated = false;
+  User user = User();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("SIGNUP")),
-      key: _scaffoldKey,
-      body: Form(
-        key: _formKey,
-        child: Column(
-          children: <Widget>[
-            TextFormField(
-              decoration: InputDecoration(labelText: "Name"),
-              onSaved: (val) {
-                _firstName = val;
-              },
-            ),
-            TextFormField(
-                decoration: InputDecoration(labelText: "Last name"),
-                onSaved: (val) {
-                  _lastName = val;
-                }),
-            TextFormField(
-              decoration: InputDecoration(labelText: "Email"),
-              onSaved: (val) {
-                _email = val;
-              },
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: "Password"),
-              onSaved: (val) {
-                _password = val;
-              },
-            ),
-            RaisedButton(
-              onPressed: () {
-                _formKey.currentState.save();
-                _signup(_firstName, _lastName, _email, _password);
-              },
-              child: Text("Create account"),
-            ),
-          ],
-        ),
-      ),
-    );
+        appBar: AppBar(title: Text("SIGNUP")),
+        key: _scaffoldKey,
+        body: this.userCreated ? _buildBackToLogin() : _buildSignupForm());
   }
 
-  Future<http.Response> _signup(firstName, lastName, email, password) async {
+  void _signup() async {
     _scaffoldKey.currentState.showSnackBar(SnackBar(
       content: Text("Creating account..."),
     ));
 
-    final response = await http.post(globals.signup_url, body: {
-      "firstName": firstName,
-      "lastName": lastName,
-      "email": email,
-      "password": password
-    }).then((response) {
-      String _message = json.decode(response.body)['message'];
+    final response = await this.user.create();
 
-      _scaffoldKey.currentState.showSnackBar(SnackBar(
-        content: Text(_message),
-      ));
-    });
-    return response;
+    String _message = json.decode(response.body)['message'];
+    _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text(_message)));
+    if (response.statusCode == 201) setState(() => this.userCreated = true);
+  }
+
+  Widget _buildSignupForm() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(labelText: "Name"),
+            onSaved: (val) {
+              this.user.firstName = val;
+            },
+          ),
+          TextFormField(
+              decoration: InputDecoration(labelText: "Last name"),
+              onSaved: (val) {
+                this.user.lastName = val;
+              }),
+          TextFormField(
+            decoration: InputDecoration(labelText: "Email"),
+            onSaved: (val) {
+              this.user.email = val;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: "Password"),
+            onSaved: (val) {
+              this.user.password = val;
+            },
+          ),
+          RaisedButton(
+            onPressed: () {
+              _formKey.currentState.save();
+              _signup();
+            },
+            child: Text("Create account"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBackToLogin() {
+    return Center(child: Text("User created ! You can now login."));
   }
 }
--- lib/user.dart
+++ lib/user.dart
@@ -5,18 +5,21 @@ import 'resolution.dart';
 import 'dart:convert';
 
 class User {
-  final String firstName;
-  final String lastName;
-  final String email;
+  String firstName;
+  String lastName;
+  String email;
+  String password;
   List<Resolution> resolutions;
 
-  User(this.firstName, this.lastName, this.email, this.resolutions);
+  User();
 
   User.fromJson(Map<String, dynamic> json)
       : firstName = json['firstName'],
         lastName = json['lastName'],
         email = json['email'],
-        resolutions = (json['resolutions'] as List).map((el) => Resolution.fromJson(el)).toList();
+        resolutions = (json['resolutions'] as List)
+            .map((el) => Resolution.fromJson(el))
+            .toList();
 
   Map<String, dynamic> toJson() => {
         'firstName': firstName,
@@ -25,7 +28,7 @@ class User {
         'resolutions': resolutions
       };
 
-  static Future<User> show(id) async {
+  static Future<User> show(String id) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String token = prefs.getString('token');
     final response = await http.get(globals.users_url + id,
@@ -33,5 +36,12 @@ class User {
     return User.fromJson(json.decode(response.body)['data']);
   }
 
-
+  Future<http.Response> create() {
+    return http.post(globals.signup_url, body: {
+      "firstName": this.firstName,
+      "lastName": this.lastName,
+      "email": this.email,
+      "password": this.password
+    });
+  }
 }
